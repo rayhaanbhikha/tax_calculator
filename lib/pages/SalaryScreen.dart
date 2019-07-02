7a02fe916ee1c899ecd4d92216d9e9f5ef985f09
Ray <rayhaan.bhikha@sky.uk>
2019-07-02 21:42:49 +0100

    list of widgets built.

--- lib/pages/SalaryScreen.dart
+++ lib/pages/SalaryScreen.dart
@@ -17,21 +17,49 @@ class SalaryPage extends StatelessWidget {
 }
 
 class SalaryTemplate extends StatelessWidget {
+  final List<Widget> widgets = <Widget>[
+    MainText(
+      questionText: "How much do you get paid?",
+      infoText: "Enter salary info",
+    ),
+    SalaryTextField(),
+    MyCard(child: MyRadioButtons()),
+    FloatingBtn()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.all(32),
-        child: ListView(
-          children: <Widget>[
-            MainText(
-              questionText: "How much do you get paid?",
-              infoText: "Enter salary info",
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            SalaryTextField(),
-            Padding(padding: EdgeInsets.all(10)),
-            MyCard(child: MyRadioButtons(),)
-          ],
+        child: ListView.separated(
+          itemCount: widgets.length,
+          itemBuilder: (context, index) {
+            return widgets[index];
+          },
+          separatorBuilder: (context, index) {
+            double padding = index == widgets.length - 2 ? 20 : 10;
+            return Padding(padding: EdgeInsets.all(padding));
+          },
         ));
+  }
+}
+
+class FloatingBtn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        FloatingActionButton(
+          elevation: 100.0,
+          child: Icon(
+            Icons.chevron_right,
+            size: 30.0,
+          ),
+          backgroundColor: Colors.black,
+          onPressed: () => print("hello world"),
+        )
+      ],
+    );
   }
 }
